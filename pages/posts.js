f78c38dd681b37ec1ff4f4d9afe9606a50c3ea4d
Lesan <1960681385@qq.com>
2022-08-09 18:39:38 +0800

    😁配置渲染markdown文件

--- pages/posts.js
+++ pages/posts.js
@@ -8,7 +8,6 @@ import { getSortedPostsData } from '../lib/posts'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
-  console.log(allPostsData)
   return {
     props: {
       allPostsData
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,20 +1,76 @@
+import { Container, Badge } from '@chakra-ui/react'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
+import { useColorModeValue } from '@chakra-ui/react'
+import Layout from '../../components/layouts/article'
+import { Title } from '../../components/post'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import {
+  oneDark,
+  oneLight
+} from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 export default function Post({ postData }) {
   return (
-    <div>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <h1>{postData.title}</h1>
-        <div>
-          <div>{postData.date}</div>
+    <Layout title={postData.title}>
+      <style jsx>{`
+        .markdown-body {
+          box-sizing: border-box;
+          min-width: 200px;
+          max-width: 980px;
+          margin: 0 auto;
+          padding: 45px;
+          background-color: transparent;
+        }
+
+        @media (max-width: 767px) {
+          .markdown-body {
+            padding: 15px;
+          }
+        }
+      `}</style>
+
+      <Container maxW="container.lg">
+        <Title>
+          {postData.title} <Badge>{postData.date}</Badge>
+        </Title>
+        <div
+          className={useColorModeValue(
+            'markdown-body-light markdown-body',
+            'markdown-body-dark markdown-body'
+          )}
+        >
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            components={{
+              // eslint-disable-next-line no-unused-vars
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || '')
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    // showLineNumbers={true}
+                    // eslint-disable-next-line react-hooks/rules-of-hooks
+                    style={useColorModeValue(oneLight, oneDark)}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  >
+                    {String(children).replace(/\n$/, '')}
+                  </SyntaxHighlighter>
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                )
+              }
+            }}
+          >
+            {postData.content}
+          </ReactMarkdown>
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </div>
+      </Container>
+    </Layout>
   )
 }
 
